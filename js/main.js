324181a09f8927122abed11944facc488ad5974b
wzcssw <wzcssw@163.com>
2018-11-08 23:27:49 +0800

    main

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
+// 全局：便于调试
+var scene;
+var camera;
+var renderer;
+
+var initThree = function(){
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.9, 1000);
+}; 
+
 $().ready(function(){
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-        var renderer = new THREE.WebGLRenderer();
-        
+        initThree();
+
         renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.position.z = 6;
         
         document.body.appendChild(renderer.domElement);
-        var geometry = new THREE.CubeGeometry(1,1,1);
-        var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        var geometry = new THREE.CubeGeometry(1,2,3);
+        var material = new THREE.MeshBasicMaterial({color: 0xFF69B4});// 骚粉
         var cube = new THREE.Mesh(geometry, material); scene.add(cube);
-        camera.position.z = 5;
+        
         function render() {
             requestAnimationFrame(render);
-            cube.rotation.x += 0.1;
-            cube.rotation.y += 0.1;
+            cube.rotation.x += 0.02;
+            cube.rotation.y += 0.01;
             renderer.render(scene, camera);
         }
         render();
