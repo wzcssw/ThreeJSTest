58315734af0a3f1e276efdf8036b5fbca1dab095
wzcssw <wzcssw@163.com>
2018-11-10 22:19:08 +0800

    add init function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-window.onload = function(){
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
+var scene;
+var camera;
+var renderer;
+
+var initThree = function(){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     document.body.appendChild(renderer.domElement);
-    var geometry = new THREE.CubeGeometry(1,1,1);
-    var material = new THREE.MeshBasicMaterial({color: 0xFF00FF});
-    var cube = new THREE.Mesh(geometry, material); scene.add(cube);
-    camera.position.z = 4;
+};
 
-    // // 监控 (不好使) 
-    // stats = new Stats();
-    // stats.domElement.style.position = 'absolute';
-    // stats.domElement.style.left = '0px';
-    // stats.domElement.style.top = '0px';
-    // document.body.appendChild(stats.domElement);
+window.onload = function(){
+    initThree();
+    var geometry = new THREE.CubeGeometry(40,20,10);
+    var material = new THREE.MeshBasicMaterial({color: 0xFF00FF});
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 80;
 
     function render() { // 自动旋转
         requestAnimationFrame(render);
@@ -27,40 +28,9 @@ window.onload = function(){
     render();
     var vun = true;
     function animation(){ // 相机运动
-        //renderer.clear();
-        if (camera.position.x > 5){
-            vun = false
-        }else if (camera.position.x < -5){
-            vun = true;
-        }
-        if (vun){
-            camera.position.x =camera.position.x +0.01;
-            renderer.render(scene, camera);
-            requestAnimationFrame(animation);
-        }else{
-            camera.position.x =camera.position.x -0.01;
-            renderer.render(scene, camera);
-            requestAnimationFrame(animation);
-        }
-        
+        camera.position.x =camera.position.x -0.5;
+        renderer.render(scene, camera);
+        requestAnimationFrame(animation);
     }
     animation();
-    // $(document).keydown(function (event) {// 手动旋转
-    //     switch (event.keyCode) {
-    //         case 37: 
-    //             cube.rotation.y -= 0.1;
-    //             break;
-    //         case 38: 
-    //             cube.rotation.x -= 0.1;
-    //             break;
-    //         case 39: 
-    //             cube.rotation.y += 0.1;
-    //             break; 
-    //         case 40:
-    //             cube.rotation.x += 0.1;
-    //             break; 
-    //     };
-    //     renderer.render(scene, camera);
-    //     return false;
-    // });
 }
