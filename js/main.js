7f97a2f038d0a9431d0672f32a0bc39d2355b359
wzcssw <wzcssw@163.com>
2018-11-10 23:39:05 +0800

    圆周运动 & 灯光

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var scene;
 var camera;
 var renderer;
+var light;
 
+// initThree initThree
 var initThree = function(){
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -13,24 +15,55 @@ var initThree = function(){
 
 window.onload = function(){
     initThree();
-    var geometry = new THREE.CubeGeometry(40,20,10);
-    var material = new THREE.MeshBasicMaterial({color: 0xFF00FF});
+    var geometry = new THREE.CubeGeometry(20,40,10);
+    var material = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 80;
+    camera.position.z = 90;
+    camera.position.y = 30;
 
-    function render() { // 自动旋转
-        requestAnimationFrame(render);
-        cube.rotation.x += 0.02;
-        cube.rotation.y += 0.01;
+    // 光源
+    yellowLight = new THREE.PointLight( 0xFFFF00, 2, 100 );
+    redLight = new THREE.PointLight( 0xFF0000, 3, 100 );
+    yellowLight.position.set( 50, 50, 70 );
+    redLight.position.set( 50, 50, -70 );
+    scene.add( yellowLight );
+    scene.add( redLight );
+
+    // 相机运动
+    var ArrayIndex = 0;
+    var pointArray = CalcaulateCirArray(); // 计算圆周运动坐标数组
+    function animation(){ 
+        camera.position.x = pointArray[ArrayIndex].x;
+        camera.position.z = pointArray[ArrayIndex].y;
         renderer.render(scene, camera);
-    }
-    render();
-    var vun = true;
-    function animation(){ // 相机运动
-        camera.position.x =camera.position.x -0.5;
-        renderer.render(scene, camera);
+        camera.lookAt(0,0,0)
         requestAnimationFrame(animation);
+        if(ArrayIndex>=360*5-1){
+            ArrayIndex=0;
+        }else{
+            ArrayIndex++;
+        }
     }
     animation();
-}
+};
+
+// CalcaulateCir 计算圆周运动坐标
+var CalcaulateCir = function(a,x0,y0,r,pi){
+    x1 = x0 + r * Math.sin( a * pi / 180.0);
+    y1 = y0 + r * Math.cos( a * pi / 180.0);
+    return {x: x1,y: y1};
+};
+
+var CalcaulateCirArray = function(){
+    var pointArray = [];
+    for (var a = 0;a < 360*5; a++){
+        var flg = 0;
+        if (a>0){
+            flg = a / 5
+        }
+        var point = CalcaulateCir(flg,0,0,90,3.1415);
+        pointArray[a] = point;
+    }
+    return pointArray;
+};
